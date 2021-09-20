57849fbf90c52d68979875e6b999ec6ec8eb6fb8
jerline01 <72594424+jerline01@users.noreply.github.com>
2021-09-20 13:45:53 +0530

    add files for pricing component with toggle master

---
+++ index.js
@@ -0,0 +1,10 @@
+
+$(".switch").on("click", function(){
+  $(".basic").text("19.99");
+  $(".professional").text("24.99");
+  $(".master").text("39.99");
+});
+
+$(".switch").on("click", function(){
+  
+});
